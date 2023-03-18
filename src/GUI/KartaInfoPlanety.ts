b5e11d1864c9daa8c97215acbed4b10c2dabde22
Filip Chytra <chytra115@gmail.com>
2023-03-18 21:26:58 +0100

    Hide tiles from Sun

--- src/GUI/KartaInfoPlanety.ts
+++ src/GUI/KartaInfoPlanety.ts
@@ -25,21 +25,28 @@ export class KartaInfoPlanety {
     private fillInfoTiles(){
         
         if(this.infoTiles){
-            const infoPlanety = DataPlanet.orbitalniPrvky[this.app.meshIdInView.name].infoPlanety;
-            document.getElementById("typPlanety")!.innerHTML = `${infoPlanety.typPlanety}`;
-            document.getElementById("pocetMesicu")!.innerHTML = `${infoPlanety.pocetMesicu}`;
-            document.getElementById("vzdalenostOdSlunce")!.innerHTML = `${infoPlanety.vzdalenostOdSlunce.hodnota} <span> ${infoPlanety.vzdalenostOdSlunce.jednotka}</span>`;
-            document.getElementById("polomer")!.innerHTML = `${infoPlanety.polomer.hodnota} <span>${infoPlanety.polomer.jednotka}</span>`;
-            document.getElementById("delkaDne")!.innerHTML = infoPlanety.delkaDne.hodnota2 && infoPlanety.delkaDne.jednotka2 
-                ? `${infoPlanety.delkaDne.hodnota} <span>${infoPlanety.delkaDne.jednotka}</span> ${infoPlanety.delkaDne.hodnota2} <span> ${infoPlanety.delkaDne.jednotka2}</span>`
-                : `${infoPlanety.delkaDne.hodnota} <span>${infoPlanety.delkaDne.jednotka}</span>`
-            document.getElementById("delkaRoku")!.innerHTML = `${infoPlanety.delkaRoku.hodnota} <span>${infoPlanety.delkaRoku.jednotka}</span>`;
+            if (this.app.meshIdInView.name !== "Slunce"){
+                this.infoTiles.classList.remove("hide");
+                const infoPlanety = DataPlanet.orbitalniPrvky[this.app.meshIdInView.name].infoPlanety;
+                document.getElementById("typPlanety")!.innerHTML = `${infoPlanety.typPlanety}`;
+                document.getElementById("pocetMesicu")!.innerHTML = `${infoPlanety.pocetMesicu}`;
+                document.getElementById("vzdalenostOdSlunce")!.innerHTML = `${infoPlanety.vzdalenostOdSlunce.hodnota} <span> ${infoPlanety.vzdalenostOdSlunce.jednotka}</span>`;
+                document.getElementById("polomer")!.innerHTML = `${infoPlanety.polomer.hodnota} <span>${infoPlanety.polomer.jednotka}</span>`;
+                document.getElementById("delkaDne")!.innerHTML = infoPlanety.delkaDne.hodnota2 && infoPlanety.delkaDne.jednotka2 
+                    ? `${infoPlanety.delkaDne.hodnota} <span>${infoPlanety.delkaDne.jednotka}</span> ${infoPlanety.delkaDne.hodnota2} <span> ${infoPlanety.delkaDne.jednotka2}</span>`
+                    : `${infoPlanety.delkaDne.hodnota} <span>${infoPlanety.delkaDne.jednotka}</span>`
+                document.getElementById("delkaRoku")!.innerHTML = `${infoPlanety.delkaRoku.hodnota} <span>${infoPlanety.delkaRoku.jednotka}</span>`;
+            }
+            else {
+                this.infoTiles.classList.add("hide");
+            }
         }
     }
     
     private initInfoPlanety(){
         if(this.title) this.title.textContent = this.app.meshIdInView.name;
         if(this.paragraf) this.paragraf.textContent = (this.app.meshIdInView.name !== "Slunce") ? DataPlanet.orbitalniPrvky[this.app.meshIdInView.name].infoPlanety?.text : this.getDefaultInfo();
+        this.fillInfoTiles();
     }
 
 
